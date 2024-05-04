a97e7fede8acce3671b5f52a9e3b1ff9d2df1699
loveth oballe <insiible@gmail.com>
2024-05-03 20:36:18 -0400

    adding app

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "example" {
     Environment = "Dev"
   }
 }
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "example-b" {
   bucket = "my-tf-b-test-bucket"
 
   tags = {
